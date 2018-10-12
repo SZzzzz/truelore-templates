94a8c56b3dae82385bb9e9b641b4e72ed4e792c8
宋泽 <songze@songzedeMacBook-Pro.local>
2018-10-12 10:45:05 +0800

    update template.

--- web/template/src/apis/BaseApi.ts
+++ web/template/src/apis/BaseApi.ts
@@ -11,45 +11,45 @@ enum HttpMethods {
   PATCH = 'patch'
 }
 
-type Tip = string | ((res: AxiosResponse) => string);
-interface NoticeConfig {
-  successTip?: Tip;
-  failTip?: Tip;
+type Tip<T> = string | ((res: AxiosResponse<T>) => string);
+interface NoticeConfig<T> {
+  successTip?: Tip<T>;
+  failTip?: Tip<{message: string}>;
 }
 
-interface BaseConfig extends NoticeConfig {
+interface BaseConfig<T> extends NoticeConfig<T>{
   url: string;
   params?: Object;
 }
 
-interface PostConfig extends BaseConfig {
-  data: Object;
+interface PostConfig<T> extends BaseConfig<T> {
+  data: object;
 }
 
 export default class BaseApi {
-  protected async get<T>(config: BaseConfig, extra?: AxiosRequestConfig) {
+  protected async get<T>(config: BaseConfig<T>, extra?: AxiosRequestConfig) {
     return this.request<T>(HttpMethods.GET, config, extra);
   }
 
-  protected async post<T>(config: PostConfig, extra?: AxiosRequestConfig) {
+  protected async post<T>(config: PostConfig<T>, extra?: AxiosRequestConfig) {
     return this.request<T>(HttpMethods.POST, config, extra);
   }
 
-  protected async delete<T>(config: BaseConfig, extra?: AxiosRequestConfig) {
+  protected async delete<T>(config: BaseConfig<T>, extra?: AxiosRequestConfig) {
     return this.request<T>(HttpMethods.DELETE, config, extra);
   }
 
-  protected put<T>(config: PostConfig, extra?: AxiosRequestConfig) {
+  protected put<T>(config: PostConfig<T>, extra?: AxiosRequestConfig) {
     return this.request<T>(HttpMethods.PUT, config, extra);
   }
 
-  protected patch<T>(config: PostConfig, extra?: AxiosRequestConfig) {
+  protected patch<T>(config: PostConfig<T>, extra?: AxiosRequestConfig) {
     return this.request<T>(HttpMethods.PATCH, config, extra);
   }
 
   private async request<T>(
     method: HttpMethods,
-    config: BaseConfig | PostConfig,
+    config: BaseConfig<T> | PostConfig<T>,
     extra: AxiosRequestConfig = {}
   ): Promise<T> {
     const requestConfig: AxiosRequestConfig = {
@@ -59,27 +59,27 @@ export default class BaseApi {
       ...extra
     };
     if (method === HttpMethods.POST || method === HttpMethods.PUT || method === HttpMethods.PATCH) {
-      requestConfig.data = (config as PostConfig).data || {};
+      requestConfig.data = (config as PostConfig<T>).data || {};
     }
 
     try {
       const result = await this.agent.request<T>(requestConfig);
       if (config.successTip) {
-        this.notice.successNotice(this.getTip(config.successTip, result));
+        this.notice.successNotice(this.getTip<T>(config.successTip, result));
       }
-      return result.data as T;
+      return result.data;
     } catch (error) {
       console.error(`api request error: ${config.url}`);
       console.info(JSON.stringify(error, null, 2));
       if (config.failTip) {
-        this.notice.errorNotice(this.getTip(config.failTip, error.response as AxiosResponse));
+        this.notice.errorNotice(this.getTip<T>(config.failTip, error.response as AxiosResponse<{message: string}>));
       }
       Raven.captureException(error);
       throw error;
     }
   }
 
-  private getTip(tip: Tip, res: AxiosResponse) {
+  private getTip<T>(tip: Tip<T>, res: AxiosResponse<T>) {
     if (typeof tip === 'string') {
       return tip;
     } else {
